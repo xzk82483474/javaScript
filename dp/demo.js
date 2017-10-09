5613fefecfad531c53bcafd484d944dbed18f3e2
xzk82483474 <82483474@qq.com>
2017-10-09 10:52:39 +0800

    Update demo.js

--- dp/demo.js
+++ dp/demo.js
@@ -10,19 +10,19 @@ if (element.src.match("bulbon"))
   {
 
 	x.innerHTML="灯泡已熄灭   "+new Date();
-  element.src="/home/xzk/桌面/i/eg_bulboff.gif";
+  element.src="/i/eg_bulboff.gif";
 	
   }
 else
   {
 x.innerHTML="灯泡已点亮   "+new Date();
-  element.src="/home/xzk/桌面/i/eg_bulbon.gif";
+  element.src="/i/eg_bulbon.gif";
 	
   }
 }
 </script>
 
-<img id="myimage" onclick="changeImage()" src="/home/xzk/桌面/i/eg_bulboff.gif">
+<img id="myimage" onclick="changeImage()" src="/i/eg_bulboff.gif">
 
 <p id="demo">点击灯泡来点亮或熄灭这盏灯</p>
 
